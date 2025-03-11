84398bbc20176ea748684c4fe68e160b1d637454
Lost4581 <lezhnevGD23@st.ithub.ru>
2025-03-11 20:41:52 +0300

    Finish CameraControl script, add buy manager script

--- Semester progect practica/Assets/Scripts 2/Cells/CameraControl.cs
+++ Semester progect practica/Assets/Scripts 2/Cells/CameraControl.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraControl : MonoBehaviour
 {
-    [SerializeField] private List<Camera> _camera ;
-    [SerializeField] private List<GameObject> _cell ;
-
-    private void ChangePriority()
+    [SerializeField] private List<GameObject> cameras;
+    [SerializeField] private GameObject _camera;
+    [SerializeField] private GameObject _button;
+    [SerializeField] private bool _isActive = false;
+    
+    public void OnMouseDown()
     {
-        /*if()
+        _button.SetActive(_isActive);
+        _camera.SetActive(true);
+        for (int i = 0; i < cameras.Count; i++)
         {
-
-        }*/
+            cameras[i].SetActive(false);
+        }
     }
 }
